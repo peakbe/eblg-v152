c6f87a2c533a80b90ba55f79f03b896cc769c842
peakbe <xpb66bfnpd@privaterelay.appleid.com>
2026-05-13 11:25:12 +0200

    Delete js/config.js

--- js/config.js
+++
@@ -1,8 +0,0 @@
-const BASE_URL = "https://eblg-dashboard-v84.onrender.com";
-
-export const ENDPOINTS = {
-    metar: `${BASE_URL}/metar`,
-    taf: `${BASE_URL}/taf`,
-    fids: `${BASE_URL}/fids`,
-    sonometers: `${BASE_URL}/sonos`
-};
