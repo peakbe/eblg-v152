df54f7721cc73c09d2f735e54ee6d295158b92b5
peakbe <xpb66bfnpd@privaterelay.appleid.com>
2026-05-13 11:24:35 +0200

    Create config.js

---
+++ backend/public/js/config.js
@@ -0,0 +1,8 @@
+const BASE_URL = "https://eblg-dashboard-v84.onrender.com";
+
+export const ENDPOINTS = {
+    metar: `${BASE_URL}/metar`,
+    taf: `${BASE_URL}/taf`,
+    fids: `${BASE_URL}/fids`,
+    sonometers: `${BASE_URL}/sonos`
+};
